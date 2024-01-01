e3d9dfe84ab6e224feb01435d65f3330720dc1d0
pongnatel <s3877819@rmit.edu.vn>
2024-01-01 12:25:33 +0700

    add notification permission request

--- app/src/main/java/com/example/poppop/MainActivity.java
+++ app/src/main/java/com/example/poppop/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.poppop;
 
+import android.Manifest;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Check for notification permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[] {Manifest.permission.POST_NOTIFICATIONS}, 1);
+            }
+        }
     }
 }
