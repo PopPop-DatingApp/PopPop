dad047c9671200cacdcc007be1046448d6039d8d
pongnatel <s3877819@rmit.edu.vn>
2024-01-01 16:15:51 +0700

    Class and utils for Chat

---
+++ app/src/main/java/com/example/poppop/Model/ChatroomModel.java
@@ -0,0 +1,54 @@
+package com.example.poppop.Model;
+
+import com.google.firebase.Timestamp;
+
+import java.util.List;
+
+public class ChatroomModel {
+    String chatroomId;
+    List<String> userId;
+    Timestamp lastMessageTimestamp;
+    String lastMessageSenderId;
+
+    public ChatroomModel() {
+    }
+
+    public ChatroomModel(String chatroomId, List<String> userId, Timestamp lastMessageTimestamp, String lastMessageSenderId) {
+        this.chatroomId = chatroomId;
+        this.userId = userId;
+        this.lastMessageTimestamp = lastMessageTimestamp;
+        this.lastMessageSenderId = lastMessageSenderId;
+    }
+
+    public String getChatroomId() {
+        return chatroomId;
+    }
+
+    public void setChatroomId(String chatroomId) {
+        this.chatroomId = chatroomId;
+    }
+
+    public List<String> getUserId() {
+        return userId;
+    }
+
+    public void setUserId(List<String> userId) {
+        this.userId = userId;
+    }
+
+    public Timestamp getLastMessageTimestamp() {
+        return lastMessageTimestamp;
+    }
+
+    public void setLastMessageTimestamp(Timestamp lastMessageTimestamp) {
+        this.lastMessageTimestamp = lastMessageTimestamp;
+    }
+
+    public String getLastMessageSenderId() {
+        return lastMessageSenderId;
+    }
+
+    public void setLastMessageSenderId(String lastMessageSenderId) {
+        this.lastMessageSenderId = lastMessageSenderId;
+    }
+}
