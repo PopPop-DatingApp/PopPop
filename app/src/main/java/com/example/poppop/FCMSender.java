e0c792dcc5ab67f6e7346dacf387b1bfecf4de51
pongnatel <s3877819@rmit.edu.vn>
2024-01-04 18:44:15 +0700

    Oauth test

--- app/src/main/java/com/example/poppop/FCMSender.java
+++ app/src/main/java/com/example/poppop/FCMSender.java
@@ -24,69 +24,6 @@ import java.util.Map;
 *
 * */
 public class FCMSender {
-
-    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    private static final String FCM_API = "https://fcm.googleapis.com/fcm/send";
-    //SERVER_KEY taken from firebase
-    private static final String SERVER_KEY = "AAAArrybYaM:APA91bFDhtZWU9ROTW69oPdP9mJfsFzWqlgQtMi7G2BX7nHEXIdERq-ORB31KJarcuo0AsAt5l7mp3DDVddWHqIZM7Jmlmye6oX5hfFBC724ulvdsXJrH-W6AP279uHIS7qS8ulPuWai"; // Replace with your Server Key
-
-//    public static void sendNotification(String deviceToken, String title, String message){
-//        OkHttpClient client = new OkHttpClient();
-//
-//        JSONObject data = new JSONObject();
-//        try {
-//            data.put("title", title);
-//            data.put("body", message);
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//            return; // Return or handle the exception as per your requirement
-//        }
-//
-//        JSONObject body = new JSONObject();
-//        try {
-//            body.put("to", deviceToken);
-//            body.put("notification", data);
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//            return; // Return or handle the exception as per your requirement
-//        }
-//
-//        RequestBody requestBody = RequestBody.create(body.toString(), JSON );
-//        Request request = new Request.Builder()
-//                .url(FCM_API)
-//                .header("Authorization", "Bearer " + SERVER_KEY)
-//                .post(requestBody)
-//                .build();
-//
-//        client.newCall(request).enqueue(new Callback() {
-//            @Override
-//            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-//                // Handle failure, such as network issues or API call errors
-//                e.printStackTrace();
-//            }
-//
-//            @Override
-//            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-//                // Handle response from FCM API
-//                if (response.isSuccessful()) {
-//                    // Request was successful (HTTP 200-299)
-//                    String responseBody = response.body().string();
-//                    System.out.println("FCM Response: " + responseBody);
-//                    // Add your custom logic here to handle the response
-//                } else {
-//                    // Request failed
-//                    System.err.println("FCM Request Failed: " + response.code() + " - " + response.message());
-//                }
-//            }
-//        });
-//    }
-
-    private void sendNotificationToOwner(Context activity,String ownerToken, String newParticipantName, String locationName) {
-        String title = "New Participant!!!";
-        String body = newParticipantName + " has joined your location: " + locationName;
-        sendPushToSingleInstance(activity, ownerToken, title, body);
-    }
-
     public static void sendPushToSingleInstance(String OauthToken,final Context activity, final String instanceIdToken, final String title, final String body) {
         final String url = "https://fcm.googleapis.com/v1/projects/poppop-datingapp/messages:send";
         // Replace YOUR_PROJECT_ID with your Firebase project ID
--- app/src/main/java/com/example/poppop/MainActivity.java
+++ app/src/main/java/com/example/poppop/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.poppop;
 
+import static android.content.ContentValues.TAG;
+import static com.example.poppop.GoogleTokenFetcher.fetchAccessToken;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,14 +20,24 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -33,12 +47,18 @@ public class MainActivity extends AppCompatActivity {
     private Button signOutBtn;
     private static final int RC_SIGN_IN = 40;
 
+    private static final List<String> SCOPES = Arrays.asList("https://www.googleapis.com/auth/cloud-platform");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            getAccessToken();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         setContentView(R.layout.activity_main);
-
         textView = findViewById(R.id.textView);
         signInBtn = findViewById(R.id.button);
         signOutBtn = findViewById(R.id.button2);
@@ -65,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
+                .requestServerAuthCode(getString(R.string.default_web_client_id), true)
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
@@ -92,7 +113,8 @@ public class MainActivity extends AppCompatActivity {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuth(account.getIdToken());
+                Log.e("qwerty", Objects.requireNonNull(task.getResult().getServerAuthCode()));
+                firebaseAuth(account.getIdToken(),task.getResult().getServerAuthCode());
             } catch (ApiException e) {
                 throw new RuntimeException(e);
             }
@@ -104,20 +126,32 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, RC_SIGN_IN);
     }
 
-    private void firebaseAuth(String idToken){
+    private void firebaseAuth(String idToken, String serverAuthCode){
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
         mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     FirebaseUser user = mAuth.getCurrentUser();
-
                     UserModel userModel = new UserModel();
                     userModel.setUserId(user.getUid());
                     userModel.setName(user.getDisplayName());
                     userModel.setProfile(user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : null);
 
                     textView.setText("Hello " + user.getDisplayName());
+//                    try {
+//                        String accessTokenResponse = fetchAccessToken(serverAuthCode);
+////                        FCMSender.sendPushToSingleInstance(accessTokenResponse,MainActivity.this,"dkfVuBqJQ2CjcPbUX8b1kO:APA91bE0O48rikT4UMZ-xUk_yxvePV39JNkM1PwKFHhOO-VJt-RvyeC5xvum4_iw6DHqNeyJwP00te5JfN8ug3h9K_f6LsWOg2jAJ02RXUuB3MTvzEkqcoFRDkeVRMvSHGvNe7zVRtwq",
+////                                "Test", "hello");
+//                        Log.e("accessTokenResponse",accessTokenResponse);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                        Log.e("qwerty", e.toString());
+//                    } catch (GeneralSecurityException e) {
+//                        Log.e("qwerty", e.toString());
+//                        throw new RuntimeException(e);
+//                    }
+
                 }
                 else{
                     Toast.makeText(MainActivity.this, "Fail to sign in", Toast.LENGTH_SHORT).show();
@@ -125,4 +159,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private static String getAccessToken() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new FileInputStream("java/com/example/poppop/assesets/service-account.json")).createScoped(SCOPES);
+        googleCredentials.refresh();
+        Log.d(TAG, "getAccessToken: " + googleCredentials.getAccessToken().getTokenValue());
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
 }
